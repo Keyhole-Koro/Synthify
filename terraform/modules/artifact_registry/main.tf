4c8c96f9b6f15105cb56eb3e9e68e1b80f5f9e97
Keyhole-Koro <korokororinthird@gmail.com>
2026-07-06 14:22:58 +0900

    feat: Add artifact registry cleanup policies

--- terraform/modules/artifact_registry/main.tf
+++ terraform/modules/artifact_registry/main.tf
@@ -7,6 +7,24 @@ resource "google_artifact_registry_repository" "repo" {
   docker_config {
     immutable_tags = false
   }
+
+  # 直近の10世代は常に保持する
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 10
+    }
+  }
+
+  # 作成から30日(2592000秒)経過したイメージは削除する
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+    condition {
+      older_than = "2592000s"
+    }
+  }
 }
 
 output "name" {
